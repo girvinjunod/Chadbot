aacb90ee96ff21a5b5ea7d4e416d97ec172e34bc
Renaldi Arlin <13519114@std.stei.itb.ac.id>
2021-04-27 18:20:05 +0700

    Refactored code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,13 @@ import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
 import BubbleChatGroup from './component/BubbleChatGroup'
 import { kmp } from './script/kmp'
+import { extractdate, extractmakul, extracttopik } from './script/extract'
 
+// Styles
 import "./App.css"
+
+// Images
+import avatar from './assets/johnnybravo.png';
 
 function App() {
     // Ref
@@ -13,33 +18,50 @@ function App() {
 
     // States
     const [chatText, setChatText] = React.useState("")
-    const [myChat, setMyChat] = React.useState([])
-    const [partnerChat, setPartnerChat] = React.useState([])
+    const [messages, setMessages] = React.useState([
+        {message: "Hey there, Momma! Let's have a chat for a bit :)", isMyChat: false},
+        {message: "How to chat with me you might asked? Just type your chat below and send it to me. I'm used to a compliment so don't worry.", isMyChat: false}
+    ])
 
     const sendChat = (text) => {
         if (text.length > 0) {
-            const temp = [...myChat, text]
-            const res = kmp(text, "test")
+            const temp = [...messages, {message: text, isMyChat: true}, {message: text, isMyChat: false}]
+            const res = kmp(text, "pat")
+            // if (res[0] === true) {
+            //     extractdate()
+            // }
 
-            setMyChat(temp)
+            setMessages(temp)
             setChatText("")
             lastChat.current?.scrollIntoView({ behavior: 'smooth' })
         }
     }
 
+    React.useEffect(() => {
+
+    }, [])
+
     return (
         <div className="main-container">
             <div className="side-header">
                 <h1>Chadbot</h1>
+                <img alt='' src={avatar} style={{width: '100%'}}/>
+                <div className="side-footer">
+                    <h2>The Team</h2>
+                    <p>Girvin Junod - 13519</p>
+                    <p>Alvin Wilta - 13519</p>
+                    <p>Renaldi Arlin - 13519114</p>
+                </div>
             </div>
 
             <div className="body">
                 <div className="chat-container">
                     <BubbleChatGroup 
-                        myChat={myChat} 
-                        partnerChat={partnerChat} 
-                        myChatColor={"green"} 
-                        partnerChatColor={"grey"}/>
+                        messages={messages} 
+                        myChatTextColor={"white"}
+                        partnerTextColor={"black"}
+                        myChatColor={"#32353f"}
+                        partnerChatColor={"#FACA00"}/>
                     <div ref={lastChat} style={{height: "64px", scrollMargin: "16px"}}/>
                 </div>
                 <div className="chat-text-box">
